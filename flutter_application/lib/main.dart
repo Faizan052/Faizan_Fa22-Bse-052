06c5163f4945c1fcf53dc64562632e6a8cc06593
Faizan052 <faizan.sultanhsphsp@gmail.com>
2025-03-03 09:04:32 +0500

    Flutter Application

---
+++ flutter_application/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(First());
+}
+class First extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Center(child: Text('Muhammad Faizan'),),);
+  }
+}
