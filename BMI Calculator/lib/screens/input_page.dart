8fe802582d12f34328d8f01d24fde7cc14af487d
Faizan052 <faizan.sultanhsphsp@gmail.com>
2025-05-04 12:58:45 +0500

    BMI_Calculator(video 12)
    In this , I create a constants file and design the height section

--- BMI Calculator/lib/screens/input_page.dart
+++ BMI Calculator/lib/screens/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'constants.dart';
 enum Gender { male, female }
 class InputPage extends StatefulWidget {
   @override
