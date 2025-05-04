022f6ba4723998e4d3a0d8e1c6a81e7e74c2061a
Faizan052 <faizan.sultanhsphsp@gmail.com>
2025-05-04 10:42:40 +0500

    BMI_Calculator(video 5)
    In this, I clean up the code by using repeated constraints to avoid long code

--- BMI Calculator/lib/screens/input_page.dart
+++ BMI Calculator/lib/screens/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
@@ -12,16 +16,24 @@ class BMICalculator extends StatelessWidget {
           title: Text('BMI Calculator'),
           backgroundColor: Color(0xFF0A0E21),
         ),
-        body: Center(
-          child: Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: Color(0xFF1D1E33),
-              borderRadius: BorderRadius.circular(10.0),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: activeCardColor,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
             ),
-            height: 200.0,
-            width: 200.0,
-          ),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            ),
+          ],
         ),
       ),
     );
